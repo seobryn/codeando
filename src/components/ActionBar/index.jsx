bac22b86ad4570f4ea0fc7cd37b69c33b94501de
Jose Joya <ing.jose.joya@gmail.com>
2024-03-11 03:19:28 -0500

    fix: Improve Alt for some images

--- src/components/ActionBar/index.jsx
+++ src/components/ActionBar/index.jsx
@@ -1,20 +1,28 @@
-import styles from './ActionBar.module.css'
-import { BtnList } from './BtnList'
+import styles from "./ActionBar.module.css"
+import { BtnList } from "./BtnList"
 
 export function ActionBar({ onSetView, currentView }) {
-    const handleChangeView = (clickedView) => {
-        if (clickedView !== currentView) {
-            onSetView(clickedView)
-        }
+  const handleChangeView = (clickedView) => {
+    if (clickedView !== currentView) {
+      onSetView(clickedView)
     }
-    return (
-        <section className={styles.container}>
-            {
-                BtnList.map((view, idx) => (
-                    <button key={`option-btn_${idx}`} className={[styles["option-btn"], currentView === view.viewName ? styles.selected : ''].join(" ").trimEnd()}
-                        onClick={() => handleChangeView(view.viewName)}><img src={view.image} /></button>
-                ))
-            }
-        </section>
-    )
+  }
+  return (
+    <section className={styles.container}>
+      {BtnList.map((view, idx) => (
+        <button
+          key={`option-btn_${idx}`}
+          className={[
+            styles["option-btn"],
+            currentView === view.viewName ? styles.selected : "",
+          ]
+            .join(" ")
+            .trimEnd()}
+          onClick={() => handleChangeView(view.viewName)}
+        >
+          <img src={view.image} alt="Opción de menú" />
+        </button>
+      ))}
+    </section>
+  )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,15 +2,25 @@ import { DonationButton } from "../DonationButton"
 import styles from "./header.module.css"
 
 export function Header({ onShare }) {
-
   return (
     <header className={styles.header}>
       <a href="/">
-        <img src="/codeando.svg" className={styles.logo} />
+        <img
+          src="/codeando.svg"
+          className={styles.logo}
+          alt="Logo de Codeando"
+        />
         <h3>Project</h3>
       </a>
-      <div className={styles['btn-bar']}>
-        <button onClick={onShare}>Share <img className={styles.icon} src="/share-icon.svg" /></button>
+      <div className={styles["btn-bar"]}>
+        <button onClick={onShare}>
+          Share{" "}
+          <img
+            className={styles.icon}
+            src="/share-icon.svg"
+            alt="Compartir Proyecto"
+          />
+        </button>
         <DonationButton />
       </div>
     </header>
