926c470dfcdcc147f0a485ba57697d5e8fb7f52c
Jose Joya <ing.jose.joya@gmail.com>
2024-03-13 11:56:05 -0500

    fix: updated donation button to use the right language

--- src/components/DonationButton/index.jsx
+++ src/components/DonationButton/index.jsx
@@ -1,20 +1,24 @@
-export function DonationButton(){
-    return (
-        <form
-        action="https://www.paypal.com/donate"
-        method="post"
-        target="_top"
-        className="donate-container"
-      >
-        <input type="hidden" name="hosted_button_id" value="RNRKC4PPWUSAL" />
-        <input
-          type="image"
-          src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
-          border="0"
-          name="submit"
-          title="PayPal - The safer, easier way to pay online!"
-          alt="Donate with PayPal button"
-        />
-      </form>
-    )
+export function DonationButton() {
+  return (
+    <form
+      action="https://www.paypal.com/donate"
+      method="post"
+      target="_top"
+      className="donate-container"
+    >
+      <input type="hidden" name="hosted_button_id" value="RNRKC4PPWUSAL" />
+      <button
+        type="submit"
+        style={{
+          border: 0,
+          backgroundColor: "transparent",
+          width: 150,
+          height: 45,
+          backgroundImage: "url(/DonationButton.svg)",
+          backgroundSize: "100% 100%",
+          cursor: "pointer",
+        }}
+      ></button>
+    </form>
+  )
 }
